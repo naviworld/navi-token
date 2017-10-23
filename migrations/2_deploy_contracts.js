467b8f6f4791c86a1979bfc621da529ebb39a583
davter <david_teruzzi@carrefour.com>
2017-10-23 15:33:24 +0200

    solc compile 0.4.15

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,18 @@
+
+const fs = require('fs');
+var CONTRACT_FILE = "./PARAMS/contract_address.txt"
+
 var NaviToken = artifacts.require("./NaviToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(NaviToken);
+    deployer.deploy(NaviToken).then(function() {   
+
+        console.log('NaviToken.address = ' + NaviToken.address)
+        fs.writeFile(CONTRACT_FILE, NaviToken.address, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file " + CONTRACT_FILE + " was saved!");
+      }); 
+    });
 };
